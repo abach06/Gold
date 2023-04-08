5a91f8ff1babd1bb773cf7bc8d502f552745d958
Aaron Bachman <abach06@gmail.com>
2023-04-08 00:38:56 +0000

    Python stuff

---
+++ Fucnctions.py
@@ -0,0 +1,11 @@
+def contact_card(name, age, car_model): #name age and car_model are parameters=(variables that we define when we are defining the function)
+    
+    return f"{name} is {age} and drives a {car_model}"
+    
+contact_card("Keith", 29, "Honda Civic")
+
+
+
+
+    
+    
